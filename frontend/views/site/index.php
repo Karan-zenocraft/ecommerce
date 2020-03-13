ed46d073f124cea554d3d5d2a8b750280c6f9403
Karan Solanki <KSolanki@ZENOSYS.LOCAL>
2020-03-13 16:30:52 +0530

    frontend changes

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,32 +1,206 @@
-<style type="text/css">
+<section class="Banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 BannerLeft">
+                    <div class="Box">
+                        <h1>App That
+                            <br>
+                            Suits You Better</h1>
+                        <p>inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards.</p>
+                        <div class="Technology">
+                            <div class="Android">
+                                <div class="Icon">
+                                    <i class="fa fa-apple" aria-hidden="true"></i>
+                                </div>
+                                <div class="Des">
+                                    <p><span>Available</span>
+                                        on App Store
+                                    </p>
+                                </div>
+                            </div>
 
-.footer{
-background-color: #222;
-border-top:1px solid #222;
-color: #9d9d9d;
-}
-.pull-left:hover{
-color: #ffff;
-}
-.pull-right:hover{
-color: #ffff;
-}
-</style><?php
 
-/* @var $this yii\web\View */
+                            <div class="Android Ios">
+                                <div class="Icon">
+                                    <i class="fa fa-android" aria-hidden="true"></i>
+                                </div>
+                                <div class="Des">
+                                    <p><span>Available</span>
+                                        on App Store
+                                    </p>
+                                </div>
+                            </div>
 
-$this->title = 'ecommerce';
-?>
-<div class="site-index">
+                        </div>
 
-    <div class="jumbotron" style="color: #ffff">
-        <h1>Welcome</h1>
+                    </div>
+                </div>
 
-        <p class="lead">To ecommerce</p>
-    </div>
+                <div class="col-md-6 BannerRight">
 
-    <div class="body-content">
+                    <div class="Mobile">
+                        <img src="<?php echo Yii::getAlias('@web') . "/themes/ecommerce_theme/image/mobile.png" ?>" alt="" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="Unique">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 Title">
+                    <h2>Unique Features</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+
+            </div>
+
+
+            <div class="row Customrow">
+                <div class="col-md-4 Box">
+                    <div class="MainBox" data-tilt>
+                        <div class="Icon fa fa-sticky-note">
+
+                        </div>
+                        <h3>Expert Technicians</h3>
+                        <p>Usage of the Internet is becoming more common due to rapid advancement of technology and power.</p>
+                    </div>
+                </div>
+
+                <div class="col-md-4 Box">
+                    <div class="MainBox">
+                        <div class="Icon fa fa-sticky-note">
+
+                        </div>
+                        <h3>Professional Service</h3>
+                        <p>Usage of the Internet is becoming more common due to rapid advancement of technology and power.</p>
+                    </div>
+                </div>
+
+                <div class="col-md-4 Box">
+                    <div class="MainBox">
+                        <div class="Icon fa fa-sticky-note">
+
+                        </div>
+                        <h3>Great Support</h3>
+                        <p>Usage of the Internet is becoming more common due to rapid advancement of technology and power.</p>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
 
-    </div>
-</div>
+    </section>
+
+    <section class="Client">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 ClientInner">
+                    <div class="wrapper" id="wrapper">
+                        <p class="">
+
+                            <span class="Quote">
+                            	 <img src="<?php echo Yii::getAlias('@web') . "/themes/ecommerce_theme/image/quote.webp" ?>" alt="" class="img-fluid">
+                                
+
+                            </span>
+
+                            Fanny Spencer
+
+                            <span class="Star"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></span>
+                            <span class="Des">
+                                As conscious traveling Paup ers we must always be oncerned about our dear Mother Earth. If you think about it,
+                                you travel
+                                across her face </span>
+                        </p>
+
+                        <p class="">
+
+                            <span class="Quote">
+                                 <img src="<?php echo Yii::getAlias('@web') . "/themes/ecommerce_theme/image/quote.webp" ?>" alt="" class="img-fluid">
+
+                            </span>
+
+                            Fanny Spencer
+
+                            <span class="Star"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></span>
+                            <span class="Des">
+                                As conscious traveling Paup ers we must always be oncerned about our dear Mother Earth. If you think about it,
+                                you travel
+                                across her face </span>
+                        </p>
+
+                        <p class="">
+
+                            <span class="Quote">
+                                 <img src="<?php echo Yii::getAlias('@web') . "/themes/ecommerce_theme/image/quote.webp" ?>" alt="" class="img-fluid">
+
+                            </span>
+
+                            Fanny Spencer
+
+                            <span class="Star"><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i></span>
+                            <span class="Des">
+                                As conscious traveling Paup ers we must always be oncerned about our dear Mother Earth. If you think about it,
+                                you travel
+                                across her face </span>
+                        </p>
+
+
+
+                        <ul class="dots-wrap">
+                            <li class="dot">
+                               <img src="<?php echo Yii::getAlias('@web') . "/themes/ecommerce_theme/image/t1.webp" ?>" alt="" class="img-fluid">
+                            </li>
+                            <li class="dot">
+                                 <img src="<?php echo Yii::getAlias('@web') . "/themes/ecommerce_theme/image/t2.webp" ?>" alt="" class="img-fluid">
+                            </li>
+                            <li class="dot">
+                                 <img src="<?php echo Yii::getAlias('@web') . "/themes/ecommerce_theme/image/t3.webp" ?>" alt="" class="img-fluid">
+
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+
+
+    <section class="Unique Screen">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 Title">
+                    <h2>Featured Screens</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+
+            </div>
+
+
+            <div class="row">
+                <div class="col-md-12 ScreenSlider">
+                    <div class="slider">
+                        <img src="<?php echo Yii::getAlias('@web') . "/themes/ecommerce_theme/image/1.png" ?>" alt="" class="img-fluid">
+                        <img src="<?php echo Yii::getAlias('@web') . "/themes/ecommerce_theme/image/2.png" ?>" alt="" class="img-fluid">
+                        <img src="<?php echo Yii::getAlias('@web') . "/themes/ecommerce_theme/image/3.png" ?>" alt="" class="img-fluid">
+                        <img src="<?php echo Yii::getAlias('@web') . "/themes/ecommerce_theme/image/4.png" ?>" alt="" class="img-fluid">
+                        <img src="<?php echo Yii::getAlias('@web') . "/themes/ecommerce_theme/image/1.png" ?>" alt="" class="img-fluid">
+                        <img src="<?php echo Yii::getAlias('@web') . "/themes/ecommerce_theme/image/2.png" ?>" alt="" class="img-fluid">
+                        <img src="<?php echo Yii::getAlias('@web') . "/themes/ecommerce_theme/image/3.png" ?>" alt="" class="img-fluid">
+                        <img src="<?php echo Yii::getAlias('@web') . "/themes/ecommerce_theme/image/4.png" ?>" alt="" class="img-fluid">
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
