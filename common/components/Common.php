3dac86e74c495d05dfadce7b3fc5811a61f7bb06
root <root@localhost.creatusello.com>
2020-03-31 15:41:22 +0530

    add IOS notification code

--- common/components/Common.php
+++ common/components/Common.php
@@ -1944,4 +1944,33 @@ class Common
         return preg_replace('/-+/', '-', $string); // Replaces multiple hyphens with single one.*/
         return preg_replace('/[^A-Za-z0-9]/', "", $string);
     }
+    public static function SendNotificationIOS()
+    {
+        $url = "https://fcm.googleapis.com/fcm/send";
+        $token = "";
+        $serverKey = '';
+        $title = "Title";
+        $body = "Body of the message";
+        $notification = array('title' => $title, 'text' => $body, 'sound' => 'default', 'badge' => '1');
+        $arrayToSend = array('to' => $token, 'notification' => $notification, 'priority' => 'high');
+        $json = json_encode($arrayToSend);
+        $headers = array();
+        $headers[] = 'Content-Type: application/json';
+        $headers[] = 'Authorization: key=' . $serverKey;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST,
+
+            "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+//Send the request
+        $response = curl_exec($ch);
+//Close request
+        if ($response === false) {
+            die('FCM Send Error: ' . curl_error($ch));
+        }
+        curl_close($ch);
+    }
 }
