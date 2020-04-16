380b9c4bcf1339d123f757d87a10fc6f42332de4
root <root@localhost.creatusello.com>
2020-04-16 16:59:14 +0530

    refund

--- api/controllers/OrdersController.php
+++ api/controllers/OrdersController.php
@@ -273,7 +273,7 @@ class OrdersController extends \yii\base\Controller
                         curl_close($cURLConnection);
 
                         $jsonArrayResponse = json_decode($phoneList);
-                        p($jsonArrayResponse->transactions);
+                        p($jsonArrayResponse->transactions[0]->related_resources[0]->authorization);
                         $payer_id = $jsonArrayResponse->cart;
                         $header = array(
                             "Content-Type: application/json",
